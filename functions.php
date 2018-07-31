8cfc6e1dbab2491d7b2925860a6fd4b07012084a
niiharamegumu <nhrmgm1223@gmail.com>
2018-07-31 21:07:45 +0900

    サイドバーにウィジェット設定追加

--- functions.php
+++ functions.php
@@ -34,3 +34,26 @@ wp_enqueue_style( 'theme-common', get_template_directory_uri() . '/css/common.cs
 }
 
 add_action('wp_enqueue_scripts', 'theme_styles');
+
+/**
+ *ウィジェット設定
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author megumu
+*/
+
+function theme_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => 'サイドバー',
+			'id'            => 'sidebar-1',
+			'description'   => '画面の右側にあるサイドバー',
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2>',
+			'after_title'   => '</h2>'
+		)
+	);
+}
+add_action( 'widgets_init', 'theme_widgets_init' );
